cc913861302562951d4a97f258c3677f4377f1cf
ShengHuang <huangshengtx@163.com>
2025-03-01 20:54:31 +0800

    remove cus code

--- merge_daily_ffmepg.py
+++ merge_daily_ffmepg.py
@@ -50,7 +50,7 @@ def merge_videos(input_folder, output_folder):
         print("未找到有效的日期键。")
     # 遍历文件夹中的所有文件
     for file_name in os.listdir(input_folder):
-        if file_name.endswith(('.mp4', '.avi', '.mov')) and not file_name.startswith('2024110608'):  # 检查文件扩展名
+        if file_name.endswith(('.mp4', '.avi', '.mov')):  # 检查文件扩展名
             key = file_name[:8]  # 获取文件名前8个字符
             current_date = datetime.strptime(key, "%Y%m%d")
             if key not in exist_videos_dict and (max_date is None or current_date > max_date):
